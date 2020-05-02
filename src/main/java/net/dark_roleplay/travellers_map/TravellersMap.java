964c26fa60999a52ccca74969ad78ed1cd2da503
JTK222 <jtk222.lukas@gmail.com>
2020-05-02 11:54:54 +0200

    Initial Forge & Mod Setup

---
+++ src/main/java/net/dark_roleplay/travellers_map/TravellersMap.java
@@ -0,0 +1,10 @@
+package net.dark_roleplay.travellers_map;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(TravellersMap.MODID)
+public class TravellersMap {
+
+    public static final String MODID = "travellers_map";
+
+}
