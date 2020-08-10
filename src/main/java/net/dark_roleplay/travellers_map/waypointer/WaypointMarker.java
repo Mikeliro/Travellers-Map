3d27e8295ba669fccd8b3f294c9456b8baa4b6ab
JTK222 <lukasporwit3@googlemail.com>
2020-08-10 15:57:33 +0200

    Started Implementing new Waypoint Mechanics

---
+++ src/main/java/net/dark_roleplay/travellers_map/waypointer/WaypointMarker.java
@@ -0,0 +1,46 @@
+package net.dark_roleplay.travellers_map.waypointer;
+
+import com.google.gson.JsonObject;
+import net.minecraft.util.JSONUtils;
+import net.minecraft.util.ResourceLocation;
+
+public class WaypointMarker {
+
+    private String internalName;
+    private String displayName;
+
+    private ResourceLocation compassIcon;
+    private ResourceLocation compassIconTint;
+
+    private ResourceLocation mapIcon;
+    private ResourceLocation mapIconColored;
+
+    public WaypointMarker(JsonObject json){
+        internalName = JSONUtils.getString(json, "name", "");
+        displayName = JSONUtils.getString(json, "displayName", internalName);
+        compassIcon = locOrNull(JSONUtils.getString(json, "compassIcon", null), null);
+        compassIconTint = locOrNull(JSONUtils.getString(json, "compassIconTint", null), null);
+        mapIcon = locOrNull(JSONUtils.getString(json, "compassIcon", null), compassIcon);
+        mapIconColored = locOrNull(JSONUtils.getString(json, "compassIconTint", null), compassIconTint);
+    }
+
+    private ResourceLocation locOrNull(String loc, ResourceLocation fallback){
+        return loc == null ? fallback != null ? fallback : null : new ResourceLocation(loc);
+    }
+
+    public ResourceLocation getCompassIcon(){
+        return compassIcon;
+    }
+
+    public ResourceLocation getCompassIconTint(){
+        return compassIconTint;
+    }
+
+    public ResourceLocation getMapIcon(){
+        return mapIcon;
+    }
+
+    public ResourceLocation getMapIconTint(){
+        return mapIconColored;
+    }
+}
--- src/main/java/net/dark_roleplay/travellers_map/waypointer/WaypointSpriteAtlasHelper.java
+++ src/main/java/net/dark_roleplay/travellers_map/waypointer/WaypointSpriteAtlasHelper.java
@@ -1,9 +1,66 @@
 package net.dark_roleplay.travellers_map.waypointer;
 
+import com.google.gson.*;
+import com.google.gson.stream.JsonReader;
+import net.dark_roleplay.travellers_map.TravellersMap;
+import net.dark_roleplay.travellers_map.listeners.ResourceReloadListener;
+import net.dark_roleplay.travellers_map.objects.huds.compass.CompassHud;
+import net.dark_roleplay.travellers_map.objects.huds.minimap.MinimapHUD;
+import net.dark_roleplay.travellers_map.objects.style.HudStyle;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.texture.AtlasTexture;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
+import net.minecraft.profiler.IProfiler;
+import net.minecraft.resources.IFutureReloadListener;
+import net.minecraft.resources.IReloadableResourceManager;
+import net.minecraft.resources.IResource;
+import net.minecraft.resources.IResourceManager;
+import net.minecraft.util.JSONUtils;
 import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.Spliterator;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executor;
+import java.util.stream.StreamSupport;
 
 public class WaypointSpriteAtlasHelper {
-	private final static AtlasTexture WAYPOINT_SPRITE_ATLAS = new AtlasTexture(new ResourceLocation("textures/atlas/waypoint_sprites.png"));
+    private static final JsonParser PARSER = new JsonParser();
+
+    private static final AtlasTexture WAYPOINT_SPRITE_ATLAS = new AtlasTexture(new ResourceLocation("textures/atlas/waypoint_sprites.png"));
+
+    private static final Set<WaypointMarker> MARKERS = new HashSet<>();
+
+    public void clientSetup(FMLClientSetupEvent event) {
+        if (Minecraft.getInstance() == null) return; //Nop out, running DataGen
+
+        if (Minecraft.getInstance().getResourceManager() instanceof IReloadableResourceManager) {
+            ((IReloadableResourceManager) Minecraft.getInstance().getResourceManager()).addReloadListener(WaypointSpriteAtlasHelper::reload);
+        }
+    }
+
+    public static CompletableFuture<Void> reload(IFutureReloadListener.IStage stage, IResourceManager resourceManager, IProfiler preparationsProfiler, IProfiler reloadProfiler, Executor backgroundExecutor, Executor gameExecutor) {
+        return CompletableFuture.<Void>supplyAsync(() -> {
+            MARKERS.clear();
+
+            try {
+                resourceManager.getAllResources(new ResourceLocation(TravellersMap.MODID, "travellers_map/waypoint_markers.json")).stream()
+                        .map(resource -> new JsonReader(new InputStreamReader(resource.getInputStream())))
+                        .map(reader -> (JsonObject) PARSER.parse(reader))
+                        .map(json -> JSONUtils.getJsonArray(json, "markerTypes", new JsonArray()))
+                        .flatMap(jsonArray -> StreamSupport.stream(jsonArray.spliterator(), false))
+                        .map(json -> new WaypointMarker((JsonObject) json))
+                        .forEach(marker -> MARKERS.add(marker));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }).thenCompose(stage::markCompleteAwaitingOthers);
+    }
 }
